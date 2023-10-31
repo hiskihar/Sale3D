4c24f5be0ef256c76cf7abe03c09cafcaade45b6
hiskihar <hiski.harkonen@gmail.com>
2023-10-31 21:48:12 +0200

    Tekstuuri vaihtuu mut tökkäsee vähä; renderer.compile ei auttanu Specular- ja bumpmapit väärin Pakka kuluu loppuun Pitäs lisää cooldown

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let cardIndex = 0;
+let loader = new THREE.TextureLoader();
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x0f1024);
 
@@ -5,24 +8,29 @@ const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement);
 
-
-
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
-
+// Set camera 5 units away from the origin
 camera.position.z = 5;
 
-// Set card shape
-const texture = new THREE.TextureLoader().load('textures/pakka@32x.png' );
-
-let frontMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('textures/x32/pakka@32x.png') });
+// Set initial textures
+let frontMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('textures/x32/patakuningas@32x.png') });
 let backMaterial  = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('textures/x32/pakka@32x.png') });
 let sideMaterial  = new THREE.MeshStandardMaterial({ color: 0xffffff });
 
+// Create an array for the front textures
+const cardTextures = [];
+for (let i = 0; i < 54; i++) {
+    cardTextures.push(
+        new THREE.TextureLoader().load(getCardImagePath(i))
+    );
+}
+
+// Create an array for all textures for the card geometry
 const materials = [
     sideMaterial,
     sideMaterial,
@@ -32,47 +40,48 @@ const materials = [
     backMaterial
 ];
 
+// Set bump and specular maps
 frontMaterial.bumpMap = new THREE.TextureLoader().load('textures/x32/pakka_bump@32x.png');
-backMaterial.bumpMap  = new THREE.TextureLoader().load('textures/x32/pakka_bump@32x.png');
+backMaterial .bumpMap = new THREE.TextureLoader().load('textures/x32/pakka_bump@32x.png');
 
 frontMaterial.specularMap = new THREE.TextureLoader().load('textures/x32/pakka_specular@32x.png');
-backMaterial.specularMap  = new THREE.TextureLoader().load('textures/x32/pakka_specular@32x.png');
+backMaterial .specularMap = new THREE.TextureLoader().load('textures/x32/pakka_specular@32x.png');
 
-frontMaterial.shininess = 70;
-backMaterial.shininess  = 70;
+frontMaterial.shininess = 40;
+backMaterial .shininess = 40;
+
+frontMaterial.bumpScale = 0.5;
+backMaterial .bumpScale = 0.5;
 
 frontMaterial.specular = new THREE.Color(0xbbccff);
-backMaterial.specular  = new THREE.Color(0xbbccff);
+backMaterial .specular = new THREE.Color(0xbbccff);
 
-const geometry = new THREE.BoxGeometry(2, 3, 0.015);
+// Set the card shape
+const geometry = new THREE.BoxGeometry(3, 4.5, 0.02);
 
-// Create two cards
+// Create the card
 const card1 = new THREE.Mesh(geometry, materials);
-//const card2 = new THREE.Mesh(geometry, materials); TODO: Add a second card
 scene.add(card1);
-//scene.add(card2);
 
-
+// Add a directional light
 const light = new THREE.DirectionalLight(0xffffff, 1.0);
-light.position.set(0, 0.5, 1);
+light.position.set(0, 0.8, 1);
 light.target.position.set(0, 0, 0);
 light.castShadow = false;
 scene.add(light);
 
+// Add an ambient light
 const ambientLight = new THREE.AmbientLight(0x888888);
 scene.add(ambientLight);
 
-//card1.rotation.x = Math.PI / 2;
-//card2.rotation.x = Math.PI / 2;
-
-let currentCard = 1;
-
-let turnPhase = 0.0;
+// Variables for the sine function controlling the turning and floating movement
+let turnPhase = 1.0;
 
 let xLevPhase = 0.0;
 let yLevPhase = 0.0;
 let zLevPhase = 0.0;
 
+// Ticker
 function animate() {
     requestAnimationFrame(animate);
 
@@ -81,27 +90,90 @@ function animate() {
 
     renderer.render(scene, camera);
 
-    if (turnPhase < 2.0) {
-        turnPhase += 0.005;
+    if (turnPhase < 1.0) {
+        turnPhase += 0.01;
     }
 
-    xLevPhase = (xLevPhase + 0.0004) % 1.0;
-    yLevPhase = (yLevPhase + 0.0003) % 1.0;
-    zLevPhase = (zLevPhase + 0.0005) % 1.0;
+    xLevPhase = (xLevPhase + 0.0008) % 1.0;
+    yLevPhase = (yLevPhase + 0.0005) % 1.0;
+    zLevPhase = (zLevPhase + 0.0006) % 1.0;
 }
 
+// Start the animation
 animate();
 
+renderer.compile(scene, camera);
+
 function turnCard(card, t) {
-    if (t <= 1) {
-        card.rotation.y = Math.PI * 0.5 * (t*t*t*t*t*t*t*t*t);
+    card.rotation.x = 0;
+    card.rotation.z = 0;
+    if (t <= 0.2005304) {
+        card.rotation.y = Math.PI * 2 * 6.77534196258 * t*t;
     } else {
-        card.rotation.y = Math.PI * 0.5 * (2 + (t-2)*(t-2)*(t-2)*(t-2)*(t-2)*(t-2)*(t-2)*(t-2)*(t-2))
+        const newT = ((9/8)*t-(9/8));
+        card.rotation.y = Math.PI * 2 * (newT*newT*newT + 1)
     }
 }
 
 function levitate(card) {
-    card.rotation.x = 0.1 * (Math.sin(2 * xLevPhase * Math.PI));
+    card.rotation.x += 0.1 * (Math.sin(2 * xLevPhase * Math.PI));
     card.rotation.y += 0.1 * (Math.sin(2 * xLevPhase * Math.PI));
-    card.rotation.z = 0.03 * (Math.sin(2 * zLevPhase * Math.PI));
+    card.rotation.z += 0.03 * (Math.sin(2 * zLevPhase * Math.PI));
+}
+
+function tryToPickCard() {
+    pickCard(cardIndex);
+}
+
+const shuffle = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+};
+
+// Create an array of integers from 0 until 54 (card indices) and shuffle them
+const deck = [];
+for (let i = 0; i < 54; i++) {deck.push(i);} shuffle(deck);
+
+function pickCard(index) {
+    turnPhase = 0;
+    setTimeout(
+        changeCard,
+        350,
+        index
+    )
+    cardIndex++;
+}
+
+function changeCard(index) {
+    frontMaterial.map = cardTextures[deck[index]];
+}
+
+function getCardImagePath(index) {
+    let path = "textures/"
+    path = path.concat("x32/")
+    if (cardIndex >= 0) {
+        switch (Math.floor(index / 13)) {
+            case 0:  path = path.concat("hertta"); break;
+            case 1:  path = path.concat("ruutu" ); break;
+            case 2:  path = path.concat("risti" ); break;
+            case 3:  path = path.concat("pata"  ); break;}
+        switch (index % 13) {
+            case 0:  path = path.concat("assa"      ); break;
+            case 1:  path = path.concat("kakkonen"  ); break;
+            case 2:  path = path.concat("kolmonen"  ); break;
+            case 3:  path = path.concat("nelonen"   ); break;
+            case 4:  path = path.concat("vitonen"   ); break;
+            case 5:  path = path.concat("kutonen"   ); break;
+            case 6:  path = path.concat("seitseman" ); break;
+            case 7:  path = path.concat("kahdeksan" ); break;
+            case 8:  path = path.concat("yhdeksan"  ); break;
+            case 9:  path = path.concat("kymmenen"  ); break;
+            case 10: path = path.concat("jatka"     ); break;
+            case 11: path = path.concat("kuningatar"); break;
+            case 12: path = path.concat("kuningas"  ); break;}
+    } else {
+        path = path.concat("pakka");
+    }
+    path = path.concat("@32x")
+    path = path.concat(".png")
+    return path;
 }
